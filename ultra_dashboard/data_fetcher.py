8b465e1a9bd17f9189cfec4498110b4f8dea69a0
Antonio Pelayo <aPelayo.py@gmail.com>
2025-01-12 20:13:40 -0800

    update: reduce repeating code

--- ultra_dashboard/data_fetcher.py
+++ ultra_dashboard/data_fetcher.py
@@ -4,61 +4,60 @@ from sqlalchemy.exc import SQLAlchemyError
 
 from ultra_dashboard.config import DATABASE_URL
 
+DATABASE_ENGINE = create_engine(DATABASE_URL)
+
+def execute_query(query):
+    """
+    Executes the given SQL query on the PostgreSQL database.
+
+    Args:
+        query (str): The SQL query to execute.
+    Returns:
+        pd.DataFrame: A DataFrame with the query results.
+    """
+    try:
+        df = pd.read_sql(query, DATABASE_ENGINE)
+    except SQLAlchemyError as e:
+        print(f"Error fetching data: {e}")
+        df = pd.DataFrame()
+    return df
+
 def get_finishers_per_year(race):
     """
     Fetches the number of finishers for each year from the PostgreSQL database.
 
+    Args:
+        race (str): The name of the race's table to query.
     Returns:
         pd.DataFrame: A DataFrame with columns "year" and "finishers".
     """
-    if not DATABASE_URL:
-        raise ValueError("DATABASE_URL environment variable not set.")
-
-    engine = create_engine(DATABASE_URL)
-
     if race == "WSER":
         table = "wser_historical_results"
 
-    query = f"""
+    return execute_query(f"""
         SELECT year, COUNT(*) AS finishers
         FROM {table}
         WHERE time != '00:00:00'
         GROUP BY year
         ORDER BY year;
-    """
+    """)
 
-    try:
-        df = pd.read_sql(query, engine)
-        return df
-    except SQLAlchemyError as e:
-        print(f"Error fetching data: {e}")
-        return pd.DataFrame(columns=["year", "finishers"])
 
 def get_winners_per_year(race):
     """
     Fetches the first place finisher for each year from the PostgreSQL database.
 
+    Args:
+        race (str): The name of the race's table to query.
     Returns:
         pd.DataFrame: A DataFrame with first place finisher data for each year.
     """
-    if not DATABASE_URL:
-        raise ValueError("DATABASE_URL environment variable not set.")
-
-    engine = create_engine(DATABASE_URL)
-
     if race == "WSER":
         table = "wser_historical_results"
 
-    query = f"""
+    return execute_query(f"""
         SELECT *
         FROM {table}
         WHERE place = 1
         ORDER BY year;
-    """
-
-    try:
-        df = pd.read_sql(query, engine)
-        return df
-    except SQLAlchemyError as e:
-        print(f"Error fetching data: {e}")
-        return pd.DataFrame(columns=["year", "finishers"])
+    """)
